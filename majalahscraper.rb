4eb932f085a098be8dccf9a5570a0d178a3d2a06
amirulabu <amirulabu@gmail.com>
2017-01-08 13:38:57 +0800

    Completed, first 50 pages

--- majalahscraper.rb
+++ majalahscraper.rb
@@ -11,23 +11,43 @@ phone_number = []
 browser = Watir::Browser.new :chrome
 
 #page.1
+page = 1
 
-browser.goto 'http://www.majalah.com/?allclassifieds'
+2.times.each do |i|
+	browser.goto 'http://www.majalah.com/?allclassifieds.page.' + page.to_s
+	
+	puts page
 
-browser.div(:id => "contentLeft").div(:class => "vertiPad").bs.each_with_index do |i,index|
-	if index < 50
-		feed_links << i.link.href
+	browser.div(:id => "contentLeft").div(:class => "vertiPad").bs.each_with_index do |i,index|
+		if index < 50
+			feed_links << i.link.href
+		end
+		print "."
 	end
-	print "."
+
+	puts "finish populate feed_links"
+
+	feed_links.each_with_index do |link,index|
+		print "."
+		browser.goto link
+		phone_number << browser.text.scan(/(?=(01|\+60))(((?:[0-9]( |-)?){7,10}[0-9]|\d{7,10}))/m).flatten - [nil] - ["-"] - ["01"] - ["0"] - [" "]
+	end
+
+	phone_number.flatten!.sort!
+
+	phone_number.each do |i|
+		i.delete!(' ')
+		i.delete!('-')
+	end
+
+	phone_number.uniq!
+
+	CSV.open("phonenumber.csv", "w") { |csv| csv << phone_number}
+	page = page + 1
+
+	puts "finish page " + page.to_s
 end
 
-feed_links.each do |link|
-	puts ". "
-	browser.goto link
-	phone_number << browser.text.scan(/(?=(01|\+60))(((?:[0-9]( |-)?){7,10}[0-9]|\d{7,10}))/m).flatten - [nil] - ["-"] - ["01"] - ["0"] - [" "]
-end
 
-
-print phone_number.flatten.sort.uniq!
 #Pry.start(binding)
 
